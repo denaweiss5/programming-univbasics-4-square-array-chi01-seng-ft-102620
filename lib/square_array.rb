cde653e989a405cd3fa47b8d6b7e588fda6adeac
Dena Neuman <denaweiss5@gmail.com>
2020-10-07 02:12:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
-while numbers = [1, 2, 3] do
-  puts square_array(numbers)
+def square_array(numbers)
+  while numbers = [1, 2, 3] do
+puts square_array(numbers)
 end
 
-while numbers= [9,10,16,25]
-  puts square_array(numbers)
+while numbers= [9,10,16,25] do
 
+  puts square_array(numbers)
+end
 end
